362d5b47725c1dfc11a32274334d008a37922c93
Anandhex <anand.patil.ap79@gmail.com>
2019-11-11 22:25:26 +0530

    intial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".btn-click").addEventListener("click", () => {
+  alert("You have always taught us to check the console.");
+  console.error("Thank you for guiding us");
+});
