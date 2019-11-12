e9ef56409018daa9901befec48c0978b81ff88a4
Anandhex <anand.patil.ap79@gmail.com>
2019-11-12 23:09:24 +0530

    console message update

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 document.querySelector(".btn-click").addEventListener("click", () => {
-  alert("You have always taught us to check the console.");
-  console.error("Thank you for guiding us");
+  alert(
+    "You have always taught us to check the console. Better check the console!!!"
+  );
+  console.error(
+    "Anjana Ma'am.  Your class was the only class that I didn't dread having to come to each day.  Thank you for being a bright spot and something to look forward to in the College day.\n -Anand & Co"
+  );
 });
