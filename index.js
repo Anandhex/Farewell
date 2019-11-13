c5c46f85576128db454778a67db90dbdc96db7ad
NasirHasanDilawar <52138399+NasirHasanDilawar@users.noreply.github.com>
2019-11-13 06:17:14 +0530

    V3.0N

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ document.querySelector(".btn-click").addEventListener("click", () => {
     "You have always taught us to check the console. Better check the console!!!"
   );
   console.error(
-    "Anjana Ma'am.  Your class was the only class that I didn't dread having to come to each day.  Thank you for being a bright spot and something to look on to in the College day.\n -Anand & Co"
+    "Anjana Ma'am.  Your class was the only class that I didn't dread having to come to each day.  Thank you for being a bright spot and something to look back on in our College days.\n -Anand & Co."
   );
 });
